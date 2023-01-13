fe098c52b74459ad56980b1483a863c411474fe8
sheetal <sheetal3638@gmail.com>
2023-01-13 20:53:19 +0530

    Exploring JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+let name = "Shree9"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <h1>Hello {name}</h1>
+      <div className="container">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, laboriosam a iusto voluptates et fugiat quasi quia quas ad! Odio sapiente perferendis nostrum possimus, tempore perspiciatis alias modi fuga deserunt!
+      </div>
+    </>
   );
 }
 
