e05f0c22f894cef4c955fb2403f0d68aaa333aaa
sheetal <sheetal3638@gmail.com>
2023-01-31 14:48:54 +0530

    #Video_05_Added Navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            Navbar
+            TextUtils
           </a>
           <button
             className="navbar-toggler"
@@ -28,42 +28,8 @@ function App() {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/">
-                  Link
+                  About
                 </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
               </li>
             </ul>
             <form className="d-flex" role="search">
